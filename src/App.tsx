66ed667160fc4ff6b4e1b64b8e03c8c780b3824f
FIRST_NAME LAST_NAME <you@example.com>
2024-10-29 14:27:40 +0100

    footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+import Footer from "./components/Footer";
+import "./App.css";
+
 function App() {
   return (
     <div>
---
+++ src/components/Footer.tsx
@@ -0,0 +1,31 @@
+function Footer() {
+  return (
+    <div className="footeri">
+      <div className="footer2">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/600px-LinkedIn_logo_initials.png"
+          alt=""
+        />
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/X_logo_2023.svg/886px-X_logo_2023.svg.png"
+          alt=""
+        />
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/2023_Facebook_icon.svg/800px-2023_Facebook_icon.svg.png"
+          alt=""
+        />
+      </div>
+      <span>Aide & Contact</span>
+      <span>Informations générales</span>
+      <span>
+        <a href="">CARS DEALER</a> pour les entreprises
+      </span>
+      <span>
+        <a href="">CARS DEALER </a> Partenaires
+      </span>
+      <span>Mentions légales </span>
+    </div>
+  );
+}
+
+export default Footer;
